603b23fdbfec58e184a3ba7b107988d8f6413727
jairoalzate98 <jairo.alzate@uptc.edu.co>
2018-06-21 14:26:29 -0500

    Se identifico cada arco con el id del juego

--- ClientGame/src/views/JPanelGame.java
+++ ClientGame/src/views/JPanelGame.java
@@ -1,5 +1,7 @@
 package views;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
@@ -10,6 +12,7 @@ import models.Goal;
 
 public class JPanelGame extends JPanel{
 
+	public static final Font FONT_UBUNTU = new Font("Ubuntu", Font.BOLD, 18);
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Goal> goalList;
 	
@@ -26,14 +29,20 @@ public class JPanelGame extends JPanel{
 		super.paint(g);
 		g.drawImage(new ImageIcon(getClass().getResource("/img/pasto.jpg")).getImage(), 0, 0, 660, 680, this);
 		for (Goal goal : goalList) {
+			g.setColor(Color.WHITE);
+			g.setFont(FONT_UBUNTU);
 			if (goal.getPosX() == 0 && goal.getPosY() == 260) {
 				g.drawImage(new ImageIcon(getClass().getResource("/img/goal Left.png")).getImage(), goal.getPosX(), goal.getPosY(), 40, 80, this);
+				g.drawString(String.valueOf(goal.getIdClient()), goal.getPosX(), goal.getPosY() + 100);
 			}else if(goal.getPosX() == 260 && goal.getPosY() == 0){
 				g.drawImage(new ImageIcon(getClass().getResource("/img/goal.png")).getImage(), goal.getPosX(), goal.getPosY(), 80, 40, this);
+				g.drawString(String.valueOf(goal.getIdClient()), goal.getPosX() - 20, goal.getPosY() + 20);
 			}else if(goal.getPosX() == 260 && goal.getPosY() == 600){
 				g.drawImage(new ImageIcon(getClass().getResource("/img/goal Down.png")).getImage(), goal.getPosX(), goal.getPosY(), 80, 40, this);
+				g.drawString(String.valueOf(goal.getIdClient()), goal.getPosX() + 100, goal.getPosY() + 20);
 			}else {
 				g.drawImage(new ImageIcon(getClass().getResource("/img/goal Right.png")).getImage(), goal.getPosX(), goal.getPosY(), 40, 80, this);
+				g.drawString(String.valueOf(goal.getIdClient()), goal.getPosX() + 20, goal.getPosY() - 10);
 			}
 		}
 	}
