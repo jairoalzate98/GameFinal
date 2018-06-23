0026a82348c6ec7247410ab408b1610d0e633f63
jairoalzate98 <jairo.alzate@uptc.edu.co>
2018-06-23 18:59:42 -0500

    Se agrego el movimiento del balon en un angulo determinado

--- ServerGame/src/models/Ball.java
+++ ServerGame/src/models/Ball.java
@@ -4,12 +4,18 @@ public class Ball implements Runnable{
 
 	private int posX;
 	private int posY;
+	private int posXLast;
+	private int posYLast;
+	private float angle;
 	private Thread thread;
 	private boolean stop;
-	
+	public static final int VELOCITY = 4;
+
 	public Ball(int posX, int posY) {
-		this.posX = posX;
-		this.posY = posY;
+		this.posX = 90;
+		this.posY = 90;
+		posXLast = 80;
+		posYLast = 80;
 		thread = new Thread(this);
 		thread.start();
 	}
@@ -19,6 +25,7 @@ public class Ball implements Runnable{
 	}
 
 	public void setPosX(int posX) {
+		this.posXLast = this.posX;
 		this.posX = posX;
 	}
 
@@ -27,13 +34,39 @@ public class Ball implements Runnable{
 	}
 
 	public void setPosY(int posY) {
+		this.posYLast = this.posY;
 		this.posY = posY;
 	}
 
 	@Override
 	public void run() {
 		while (!stop) {
-			
+			try {
+				Thread.sleep(60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			angle = getAngle();
+			verifyAngle();
 		}
+	}
+
+	private void verifyAngle() {
+		posX += VELOCITY * Math.cos(angle * Math.PI /180);
+		posY += VELOCITY * Math.sin(angle * Math.PI /180);
+		if (posX < 0 || posX > 591) {
+			angle = 180 - angle;   
+		}
+		else if (posX < 0 ||posX > 591) {
+			angle = 360 - angle; 
+		}
+	}
+
+	public float getAngle() {
+		float angle = (float) Math.toDegrees(Math.atan2(posY - posYLast, posX - posXLast));
+		if(angle < 0){
+			angle += 360;
+		}
+		return angle;
 	}
 }
